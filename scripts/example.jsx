68f025c0b19a2a9c581efb30e15d619ccbf6fe29
Justin Gordon <justin.gordon@gmail.com>
2014-08-29 20:08:26 -1000

    Remove switching ports to enable hot-reload
    * Add the ajax stubs to the WebpackDevServer
    * Remove the 3001 port reference, as well as the cors reference
    * Change the name of the webpack.config file to reflect it changes the
      config with the reload. Since that might not be desirable in some
      circumstances, such as using the bundle.js on another server, it makes
      sense to explicitly name the file.

--- scripts/example.jsx
+++ scripts/example.jsx
@@ -4,6 +4,6 @@ var CommentBox = require('./CommentBox'),
     React = require('react');
 
 React.renderComponent(
-  <CommentBox url="http://localhost:3001/comments.json" pollInterval={2000} />,
+  <CommentBox url="comments.json" pollInterval={2000} />,
   document.getElementById('content')
 );
--- server.js
+++ server.js
@@ -1,36 +1,32 @@
-var express = require('express');
 var bodyParser = require('body-parser');
-var app = express();
 var webpack = require('webpack');
 var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
-var cors = require('cors');
+var config = require('./webpack.hot.config');
 
 var comments = [{author: 'Pete Hunt', text: 'Hey there!'},
   {author: 'Justin Gordon', text: 'Aloha from @railsonmaui'}
 ];
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(cors());
+var server = new WebpackDevServer(webpack(config), {
+  publicPath: config.output.publicPath,
+  hot: true
+})
 
-app.get('/comments.json', function(req, res) {
+server.app.use(bodyParser.json());
+server.app.use(bodyParser.urlencoded({extended: true}));
+
+server.app.get('/comments.json', function(req, res) {
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(comments));
 });
 
-app.post('/comments.json', function(req, res) {
+server.app.post('/comments.json', function(req, res) {
   comments.push(req.body);
   res.setHeader('Content-Type', 'application/json');
   res.send(JSON.stringify(comments));
 });
 
-app.listen(3001);
-
-new WebpackDevServer(webpack(config), {
-  publicPath: config.output.publicPath,
-  hot: true
-}).listen(3000, 'localhost', function (err, result) {
+server.listen(3000, 'localhost', function (err, result) {
   if (err) {
     console.log(err);
   }
